60afcb11e8907ab4612a03a5a24d98ab8dfcd44f
DESKTOP-0GGIR90\Charly <carlosdesiderio96@gmail.com>
2023-08-26 11:57:38 -0300

    initial scaffolding

---
+++ src/main/java/org/tptacs/presentation/controllers/TestController.java
@@ -0,0 +1,16 @@
+package org.tptacs.presentation.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
