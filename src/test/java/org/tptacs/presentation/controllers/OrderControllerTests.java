20e323e39f0945a282a6c2aeb5da99376a306076
MatiasPardo <djmatiaspardo@gmail.com>
2023-09-03 00:34:18 -0300

    Add test controller orders

---
+++ src/test/java/org/tptacs/presentation/controllers/OrderControllerTests.java
@@ -0,0 +1,93 @@
+package org.tptacs.presentation.controllers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.tptacs.application.useCases.AddItemToOrderUC;
+import org.tptacs.application.useCases.CreateOrderUC;
+import org.tptacs.application.useCases.GetItemsFromOrderUC;
+import org.tptacs.presentation.requestModels.ItemOrderRequest;
+import org.tptacs.presentation.requestModels.OrderRequest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CreateOrderUC createOrderUC;
+
+    @MockBean
+    private AddItemToOrderUC addItemToOrderUC;
+
+    @MockBean
+    private GetItemsFromOrderUC getItemsFromOrderUC;
+
+    @BeforeEach
+    void setUp() {
+        // Configura comportamiento de los mocks según sea necesario
+        Mockito.when(createOrderUC.createOrder(Mockito.any(OrderRequest.class)))
+               .thenReturn("123"); // Simula una respuesta exitosa
+    }
+
+    @Test
+    public void testCreateOrderSuccess() throws Exception {
+        OrderRequest request = new OrderRequest(1L,List.of(new ItemOrderRequest("123",1L)));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Disabled
+    public void testCreateOrderFailureEmptyField() throws Exception {
+        OrderRequest request = new OrderRequest(1L, Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is(422)); // Espera un 422 debido a campo vacío
+    }
+    
+    @Test
+    @Disabled
+    public void testCreateItemFailureEmptyField() throws Exception {
+        ItemOrderRequest request = new ItemOrderRequest(null, 1L);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/orders/123/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is(422)); // Espera un 422 debido a campo vacío
+    }
+
+    @Test
+    public void testGetItemsSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/orders/123/items")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
