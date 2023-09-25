2b424c9861b68002c892aeea0f8f2ce9c9164e8e
DESKTOP-0GGIR90\Charly <carlosdesiderio96@gmail.com>
2023-09-24 22:56:41 -0300

    add navbar

---
+++ front/src/components/navbar/Navbar.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { User } from '../../interfaces/User';
+import { user } from '../../services/UsersApiClient'
+
+export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [ loggedUser, setLoggedUser ] = useState<User>(user.user)
+    
+    useEffect(() => {
+        const updateLoggedUser = (newUser: User) => {
+            setLoggedUser(newUser);
+        }
+        user.onUserChanges = updateLoggedUser;
+    },[])
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Home
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-item nav-link  ${ isActive ? 'active':'' }` }
+                        to="/products"
+                    >
+                        Productos
+                    </NavLink>
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-item nav-link  ${ isActive ? 'active':'' }` }
+                        to="/orders"
+                    >
+                        Pedidos
+                    </NavLink>
+                    
+
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+                   
+                    <span className="nav-item nav-link text-primary">
+                        {loggedUser.userName}
+                    </span>
+
+                    <button
+                        className="nav-item nav-link btn"
+                        onClick={ onLogout }
+                    >
+                        Logout
+                    </button>
+
+                </ul>
+            </div>
+        </nav>
+    )
+}
