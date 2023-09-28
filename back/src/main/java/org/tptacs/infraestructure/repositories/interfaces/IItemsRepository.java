a70f79aaa6cc68aecdac2e3eb3e69099de4b2b49
DESKTOP-0GGIR90\Charly <carlosdesiderio96@gmail.com>
2023-09-28 08:11:33 -0300

    add endpoint to render items page with order_id

--- back/src/main/java/org/tptacs/infraestructure/repositories/interfaces/IItemsRepository.java
+++ back/src/main/java/org/tptacs/infraestructure/repositories/interfaces/IItemsRepository.java
@@ -2,8 +2,11 @@ package org.tptacs.infraestructure.repositories.interfaces;
 
 import org.tptacs.domain.entities.Item;
 
+import java.util.List;
+
 public interface IItemsRepository {
     void save(Item item);
     Item get(String id);
     void exists(String id);
+    List<Item> getAll();
 }
---
+++ back/src/main/java/org/tptacs/presentation/controllers/bff/ItemsController.java
@@ -0,0 +1,28 @@
+package org.tptacs.presentation.controllers.bff;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.tptacs.application.useCases.GetItemsFromOrderUC;
+import org.tptacs.application.useCases.bff.GetProductsWithOrder;
+import org.tptacs.domain.entities.ItemOrder;
+import org.tptacs.presentation.controllers.BaseController;
+
+import java.util.List;
+
+@RestController
+@Tag(name = "Orders")
+@RequestMapping(value = "/api/items")
+public class ItemsController extends BaseController {
+
+    private final GetProductsWithOrder getProductsWithOrder;
+
+    public ItemsController(GetProductsWithOrder getProductsWithOrder) {
+        this.getProductsWithOrder = getProductsWithOrder;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ItemOrder>> getProducts(@RequestParam(value = "order_id", required = false) String orderId) {
+        return ResponseEntity.ok(getProductsWithOrder.getProductsWithOrder(orderId));
+    }
+}
