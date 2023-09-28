253fc70002539dd48dc3ddfb65c3e8369eb1a1af
ArielPiwko <arielpiwko@gmail.com>
2023-09-27 21:42:43 -0300

    creating products view and Product component

--- front/src/router/AppRouter.tsx
+++ front/src/router/AppRouter.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Login, Register } from '../auth' 
+import { Products } from '../views'
+
 export const AppRouter = () => {
   return (
     <>
@@ -7,6 +9,7 @@ export const AppRouter = () => {
         <Route path="/*" element={<Login/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/productos" element={<Products />} />
         </Routes>
     </>
   )
