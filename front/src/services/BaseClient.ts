4f03e57475b68cec276e4ab77f1cd024cd27a66b
comejia <comejia@teco.com.ar>
2023-11-06 18:23:03 -0300

    Parametrizar API URL

--- front/src/services/BaseClient.ts
+++ front/src/services/BaseClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/'
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/` || 'http://localhost:8080/api/'
 
 export const instance = axios.create({
   baseURL: BASE_URL,
